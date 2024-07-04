3bc4482f136312b7e5570779edc82799202fb69f
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-07-04 20:24:56 +0600

    added correction in education

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -7,21 +7,22 @@ const Education = () => {
     return (
         <div id="education">
             <SectionTitle heading={"My Education"}></SectionTitle>
+            <div className="flex gap-4">
             <Card className="w-full max-w-[26rem] shadow-lg">
                 <CardHeader floated={false} color="blue-gray">
                 {/* <img src={image} alt="ui/ux review check" /> */}
                         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
                 </CardHeader>
                 <CardBody>
-                        <div className="mb-3 flex items-center justify-between">
+                        <div className="mb-3 flex flex-col justify-between gap-2">
 
-                        <Typography variant="h5" color="blue-gray" className="font-medium">
+                        <Typography variant="h6" color="blue-gray" className="font-medium">
                             2024-Present
                         </Typography>
                         <Typography variant="h5" color="blue-gray" className="font-medium">
                             Masters in Information and Communication Engineering
                         </Typography>
-                        <Typography variant="h5" color="blue-gray" className="font-medium">
+                        <Typography variant="h6" color="blue-gray" className="font-medium">
                             Bangladesh University of Professionals
                         </Typography>
                         </div>
@@ -29,26 +30,24 @@ const Education = () => {
             
             </Card>
             <Card className="w-full max-w-[26rem] shadow-lg">
-                <CardHeader floated={false} color="blue-gray">
-                {/* <img src={image} alt="ui/ux review check" /> */}
-                        <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60 " />
-                </CardHeader>
-                <CardBody className="flex flex-col">
-                        <div className="mb-3 flex items-center justify-between">
+                <CardBody>
+                        <div className="mb-3 flex flex-col justify-between gap-2">
 
-                        <Typography variant="h5" color="blue-gray" className="font-medium">
-                            2024
+                        <Typography variant="h6" color="blue-gray" className="font-medium">
+                            2020-2024
                         </Typography>
                         <Typography variant="h5" color="blue-gray" className="font-medium">
                             Bachelors in Information and Communication Engineering
                         </Typography>
-                        <Typography variant="h5" color="blue-gray" className="font-medium">
+                        <Typography variant="h6" color="blue-gray" className="font-medium">
                             Bangladesh University of Professionals
                         </Typography>
                         </div>
                 </CardBody>
             
             </Card>
+            </div>
+            
         </div>
     );
 };
